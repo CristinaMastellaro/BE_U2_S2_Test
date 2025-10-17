fe5e65367915295a5e8f56a22819f73074ffb9a6
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-17 09:57:13 +0200

    create repo

---
+++ src/main/java/cristinamastellaro/BE_U2_S2_Test/BeU2S2TestApplication.java
@@ -0,0 +1,13 @@
+package cristinamastellaro.BE_U2_S2_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2S2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2S2TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cristinamastellaro/BE_U2_S2_Test/BeU2S2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package cristinamastellaro.BE_U2_S2_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2S2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
