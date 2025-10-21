ec188915335e132fb9114c27dbcd242054957986
Cristina Mastellaro <cristinamastellaro8@gmail.com>
2025-10-21 17:01:18 +0200

    wip

--- src/main/java/cristinamastellaro/BE_U2_S2_Test/services/LoginService.java
+++ src/main/java/cristinamastellaro/BE_U2_S2_Test/services/LoginService.java
@@ -6,6 +6,7 @@ import cristinamastellaro.BE_U2_S2_Test.payloads.LoginDTO;
 import cristinamastellaro.BE_U2_S2_Test.security.JWTTools;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +16,12 @@ public class LoginService {
     private EmployeeService eSer;
     @Autowired
     private JWTTools jwtTools;
+    @Autowired
+    private PasswordEncoder bcrypt;
 
     public String checkInfoAndGenerateToken(LoginDTO payload) {
         Employee found = eSer.findEmployeeByEmail(payload.email());
-        if (found.getPassword().equals(payload.password())) {
+        if (bcrypt.matches(payload.password(), found.getPassword())) {
             return jwtTools.createToken(found.getId());
         } else {
             throw new UnauthorizedException();
